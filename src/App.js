059dfb6bb690d832996fe5dc053a8b9a9bba1803
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-10-03 00:28:22 -0700

    Create Header of the Site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import Header from "./components/Header/Header";
 
 
 function App() {
   return (
-    
+      <>
+      <Header />
       <div className="app">
        
-      </div>
+       </div>
+      </>    
+      
       
   );
 }
