ea5f496b1e1f4e634a4f0e7986b87c399f7d2b1e
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-10-04 00:56:39 -0700

    Create Home (Trending) Page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import SimpleBottomNavigation from "./components/MainNav";
-import Movies from "./Pages/Movies/Movies";
-import Series from "./Pages/Series/Series";
+// import Movies from "./Pages/Movies/Movies";
+// import Series from "./Pages/Series/Series";
 import Trending from "./Pages/Trending/Trending";
-import Search from "./Pages/Search/Search";
+// import Search from "./Pages/Search/Search";
 import { Container } from "@material-ui/core";
 
 function App() {
@@ -16,9 +16,9 @@ function App() {
       <Container>
         <Switch>
           <Route path="/" component={Trending} exact />
-          <Route path="/movies" component={Movies} />
+          {/* <Route path="/movies" component={Movies} />
           <Route path="/series" component={Series} />
-          <Route path="/search" component={Search} />
+          <Route path="/search" component={Search} /> */}
         </Switch>
       </Container>
     </div>
