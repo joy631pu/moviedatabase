603e403ccb5ad3c7e3f7b13a5b0dbc44455f7de0
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-10-11 02:05:08 -0700

    Create movies page

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -1,1 +1,48 @@
-// Under development
+import axios from "axios";
+import { useEffect, useState } from "react";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+
+const Movies = () => {
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+ 
+  const fetchMovies = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=e059c3ea4e8095a63a5b575719990b02&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+    );
+    setContent(data.results);
+    
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchMovies();
+    // eslint-disable-next-line
+  }, [page]);
+
+  return (
+    <div>
+      <span className="pageTitle">Discover Movies</span>
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type="movie"
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
+     
+      <CustomPagination setPage={setPage} />
+    
+    </div>
+  );
+};
+
+export default Movies;
