c3095a083a5c3eddcefe2cff47b9e81b47049ad9
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-10-11 02:28:42 -0700

    Create tv series page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import SimpleBottomNavigation from "./components/MainNav";
 import Movies from "./Pages/Movies/Movies";
-// import Series from "./Pages/Series/Series";
+import Series from "./Pages/Series/Series";
 import Trending from "./Pages/Trending/Trending";
 // import Search from "./Pages/Search/Search";
 import { Container } from "@material-ui/core";
@@ -19,8 +19,8 @@ function App() {
           {/* Pages */}
           <Route path="/" component={Trending} exact />
           <Route path="/movies" component={Movies} />
-          {/* <Route path="/series" component={Series} />
-          <Route path="/search" component={Search} /> */}
+          <Route path="/series" component={Series} />
+          {/* <Route path="/search" component={Search} /> */}
         </Switch>
       </Container>
     </div>
--- src/Pages/Series/Series.js
+++ src/Pages/Series/Series.js
@@ -1,1 +1,49 @@
-// Under development
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+import SingleContent from "../../components/SingleContent/SingleContent";
+
+
+const Series = () => {
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  
+  
+
+  const fetchSeries = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/discover/tv?api_key=e059c3ea4e8095a63a5b575719990b02&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+    );
+    setContent(data.results);
+   };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchSeries();
+    // eslint-disable-next-line
+  }, [page]);
+
+  return (
+    <div>
+      <span className="pageTitle">Discover Series</span>
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type="tv"
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
+      <CustomPagination setPage={setPage} />
+      
+    </div>
+  );
+};
+
+export default Series;
