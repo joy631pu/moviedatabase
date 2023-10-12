d764919a55326904eaf16e87497f692f4a0f5f07
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-10-11 21:34:32 -0700

    Apply genre on tv series page

--- src/Pages/Series/Series.js
+++ src/Pages/Series/Series.js
@@ -1,31 +1,42 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Genres from "../../components/Genres/Genres";
 import CustomPagination from "../../components/Pagination/CustomPagination";
 import SingleContent from "../../components/SingleContent/SingleContent";
-
+import useGenre from "../../hooks/useGenre";
 
 const Series = () => {
+  const [genres, setGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
   const [page, setPage] = useState(1);
   const [content, setContent] = useState([]);
-  
-  
+  const genreforURL = useGenre(selectedGenres);
 
   const fetchSeries = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/discover/tv?api_key=e059c3ea4e8095a63a5b575719990b02&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+      `https://api.themoviedb.org/3/discover/tv?api_key=e059c3ea4e8095a63a5b575719990b02&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
     );
     setContent(data.results);
-   };
+    // console.log(data);
+  };
 
   useEffect(() => {
     window.scroll(0, 0);
     fetchSeries();
     // eslint-disable-next-line
-  }, [page]);
+  }, [genreforURL, page]);
 
   return (
     <div>
       <span className="pageTitle">Discover Series</span>
+      <Genres
+        type="tv"
+        selectedGenres={selectedGenres}
+        setSelectedGenres={setSelectedGenres}
+        genres={genres}
+        setGenres={setGenres}
+        setPage={setPage}
+      />
       <div className="trending">
         {content &&
           content.map((c) => (
@@ -40,7 +51,8 @@ const Series = () => {
             />
           ))}
       </div>
-      <CustomPagination setPage={setPage} />
+      
+        <CustomPagination setPage={setPage} />
       
     </div>
   );
