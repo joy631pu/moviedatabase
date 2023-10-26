7038aded595c7cdd6901ea21e4a6538b4dd4bfc4
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-10-25 23:44:09 -0700

    Create Signin with Google button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,32 @@
 import "./Header.css";
+
+import React from 'react';
+import GoogleSignInButton from '../../GoogleSignInButton';
+
 // Header Component
 const Header = () => {
+  const handleSuccess = (userData) => {
+    console.log('User successfully signed in:', userData);
+    // You can handle the user data here, e.g., store it in your state.
+  };
+
+  const handleFailure = () => {
+    console.log('Google Sign-In failed.');
+    // Handle sign-in failure here.
+  };
   return (
+    <>
     <span onClick={() => window.scroll(0, 0)} className="header">
       MovieDataBase 
+      <div className="button">    
+      <GoogleSignInButton onSuccess={handleSuccess} onFailure={handleFailure} />
+      </div>
     </span>
+    
+  </>
+
+
+   
   );
 };
 
