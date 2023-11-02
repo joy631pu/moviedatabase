4e3546bfdbab37be3611b27ccbe03446bec33f2d
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-11-02 05:10:13 -0700

    Create firebase configuration for authentication

---
+++ src/auth/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBVBlldATILgGTUuQN_J_YHJxZTuvhGE2U",
+
+    authDomain: "joy9161.firebaseapp.com",
+  
+    projectId: "joy9161",
+  
+    storageBucket: "joy9161.appspot.com",
+  
+    messagingSenderId: "325290723940",
+  
+    appId: "1:325290723940:web:c5d1c522790badd4fb2d71",
+  
+};
+
+
