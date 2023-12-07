8520eee9a69cdeba9fca88f81c424469655ef728
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-12-06 21:46:04 -0800

    Include watched component

--- src/Pages/Watchlist/WatchListReal.js
+++ src/Pages/Watchlist/WatchListReal.js
@@ -4,6 +4,7 @@ import "./WatchListReal.css";
 import Header from "./components/Header";
 import Add from "./components/Add";
 import WatchList from "./components/WatchList";
+import Watched from "./components/Watched";
 import { GlobalContextProvider } from "./context/GlobalState";
 
 const WatchListReal = () => {
@@ -12,7 +13,9 @@ const WatchListReal = () => {
       <BrowserRouter>
         <Header />
         <Switch>
+          <Route exact path="/watchlist" component={WatchList} />
           <Route exact path="/" component={WatchList} />
+          <Route exact path="/watched" component={Watched} />
           <Route exact path="/add" component={Add} />
         </Switch>
       </BrowserRouter>
