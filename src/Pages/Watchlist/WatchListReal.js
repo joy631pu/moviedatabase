00b4159c2e53188120f52661a567dbef5b24b4f6
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-12-06 20:06:48 -0800

    Create context for watchlist function

--- src/Pages/Watchlist/WatchListReal.js
+++ src/Pages/Watchlist/WatchListReal.js
@@ -2,15 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./WatchListReal.css";
 import Header from "./components/Header";
-
+import { GlobalContextProvider } from "./context/GlobalState";
 
 const WatchListReal = () => {
   return (
-    
+    <GlobalContextProvider>
       <BrowserRouter>
         <Header />
       </BrowserRouter>
-   
+    </GlobalContextProvider>
   );
 };
 
